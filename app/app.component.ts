99a501c8a730f69b6d13d732d583fb315a0b6f51
Jesse Pinho <jesse@jessepinho.com>
2016-03-28 17:13:05 -0700

    chapter6

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,10 +1,22 @@
 import {Component} from 'angular2/core';
-import {CarFormComponent} from './car-form.component';
+import {TemperatureConverterPipe} from './temperature-converter.pipe';
 
 @Component({
   selector: 'my-app',
-  template: '<car-form></car-form>',
-  directives: [CarFormComponent]
+  template: `
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title">TemperatureConverterPipe</h3>
+      </div>
+      <div class="panel-body">
+        Temperature: {{32 | temperatureConverter:'F2C':true}}
+        <hr>
+        Temperature: {{17 | temperatureConverter:'C2F':true}}
+      </div>
+    </div>
+  `,
+  pipes: [TemperatureConverterPipe]
 })
 export class AppComponent {
+  collection: string[] = ['One', 'Two', 'Three', 'Four', 'Five'];
 }
